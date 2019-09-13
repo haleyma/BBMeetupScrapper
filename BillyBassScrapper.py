c975a0871cc38df23364301cd1babc5966d25517
haleyma <haleyma@earthlink.net>
2019-09-12 22:06:10 -0400

    added comments to code

--- BillyBassScrapper.py
+++ BillyBassScrapper.py
@@ -1,3 +1,12 @@
+#  Python script to contact meetup.com api and pull down events data for Decatur Makers
+#  URL params obtained from meetup.com api:  https://www.meetup.com/meetup_api/
+#  datetime manipulation, file writing code developed with the help of 
+#  Automate the boring stuff with Python by Al Sweigart
+#  https://automatetheboringstuff.com/
+#  This code writes the info to events.txt which is read by Billy_bass_button.py
+#  Festival text-to-speech then reads the events.txt file
+#  http://festvox.org/festival/
+#  Maureen Haley , 9/12/19
 
 
 
@@ -10,11 +19,14 @@ import sys
 import io
 import datetime
 
+# current days datetime object converted to string in the iso 8601 format required by meeptup api
 currentTime = ((datetime.datetime.now()).strftime('%Y-%m-%dT%H:%M:%S')) + '.000'
 print(currentTime)
+#  timedelta used to get string for day one week in the future
 endDate = (datetime.datetime.now() + (datetime.timedelta(days=8))).strftime('%Y-%m-%dT') + '00:00:00.000'
 print(endDate)
 
+#  only= params to get key:value pairs for date, time, event name
 url ='https://api.meetup.com/Decatur-Makers/events?&sign=true&photo-host=public&no_later_than=' + endDate + '&no_earlier_than=' + currentTime + '&only=local_date,local_time,name'
 
 response = requests.get(url)
@@ -25,13 +37,25 @@ data = response.json()
 
 print(len(response.json()))
 print(data)
+
+#  write values for each event to the events.txt file
+#  format the date/time so that it will sound good spoken by the Festival package
+#  use "w" to overwrite previous info in events.txt
 f = open("events.txt", "w")
 for i in range(0,len(response.json())):
- #  f.write(data[i]['local_date'])
-#   f.write(data[i]['local_time'])
-   f.write(data[i]['name'])
+   ld = data[i]['local_date'] 
+   lt = data[i]['local_time']
+   eD = ld + ' ' + lt
+   print(eD)
+   eventDate = datetime.datetime.strptime(eD, '%Y-%m-%d %H:%M')
+   eventString = eventDate.strftime('%A %B %d %I:%M %p')
+   f.write('\n' + ', ,' + eventString)
+  # f.write('\n' + data[i]['local_time'])
+   f.write('\n' + data[i]['name'] + ',')
+   f.write('\n')
 f.close()
 
+#  print the events.txt file to the terminal just to check how it looks
 f = open("events.txt", "r")
 print(f.read())
 #print len(response.json())
--- Billy_bass_button.py
+++ Billy_bass_button.py
@@ -1,8 +1,18 @@
+# This script is adapted from the Adafruit Tutorial "Playing sounds and
+# using buttons with Raspberry Pi" by Michael Sklar
+# The code is located at https://raw.githubusercontent.com/adafruit/Adafruit_Learning_System_Guides/master/Playing_Sounds_and_Using_Buttons_with_Raspberry_Pi/audio-button.py
+# It has been modified to allow text-to-speech by a Billy Bass toy by using the events.txt file created 
+# a python script that pulls events data down from a meetup site.
+# Festival text-to-speech package:  http://festvox.org/festival/
+# From the RPi command line run: sudo apt-get install festival -y
+# Maureen Haley, 9/12/19
+
 import time
 import os
 import board
 import digitalio
 
+# set up button on RPi zero W GPIO pin 23
 button1 = digitalio.DigitalInOut(board.D23)
 button1.direction = digitalio.Direction.INPUT
 button1.pull = digitalio.Pull.UP
@@ -15,14 +25,19 @@ button1.pull = digitalio.Pull.UP
 #button3.direction = digitalio.Direction.INPUT
 #button3.pull = digitalio.Pull.UP
 
-currentDate = time.localtime()
+
 print("press a button!")
 
 while True:
     
     if not button1.value:
+        # pipe text to festival
         os.system('echo "I, , , Today is " | festival --tts')
-        os.system('currentDate | festival --tts')
+	# use date to get todays date and time and pipe to festival
+        os.system('date "+%A %B %d %Y %I:%M %P" | festival --tts')
+       # os.system('date "+%H%M" | festival --tts')
+        os.system('echo "I, , , Upcoming Events at Decatur Makers from Meetup dot com,  include, ,  "| festival --tts')
+       # read events.txt with festival
         os.system('festival --tts events.txt')
 
 #    if not button2.value:
